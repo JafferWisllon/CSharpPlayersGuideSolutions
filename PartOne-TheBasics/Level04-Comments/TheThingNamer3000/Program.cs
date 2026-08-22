fbf4a545ed8ecc1f8da8e029ff7c3f317b97d90e
Jaffer Wisllon <contatojaffer@gmail.com>
2026-08-22 11:20:15 -0300

    added solution about comments level 4

---
+++ PartOne-TheBasics/Level04-Comments/TheThingNamer3000/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); // get thing type
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine(); // get description
+string c = "Doom";
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
+
+/*
+ * Answer this question: Aside from comments, what else could you do to make this code more
+   understandable?
+   
+   Improve variables names a, b, c, d
+ */
