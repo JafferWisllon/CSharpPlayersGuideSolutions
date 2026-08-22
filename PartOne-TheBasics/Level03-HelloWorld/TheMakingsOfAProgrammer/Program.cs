7182e723a88378342dc4e5d476af33ece1cfb5f2
Jaffer Wisllon <contatojaffer@gmail.com>
2026-08-22 11:14:18 -0300

    added solutions level 3

---
+++ PartOne-TheBasics/Level03-HelloWorld/TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hi Ritlin and friends");
+Console.WriteLine("It's nice to meet you!");
+Console.WriteLine("C# is amazing programming language");
+Console.WriteLine("I will stop Uncoded One");
+Console.WriteLine("Trust me");
