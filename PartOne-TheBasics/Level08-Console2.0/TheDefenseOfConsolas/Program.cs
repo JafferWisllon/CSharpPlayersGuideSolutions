4314f695e81544b82668375fbf17341fa66b7fda
Jaffer Wisllon <contatojaffer@gmail.com>
2026-08-24 20:31:32 -0300

    added solutions level 8 - Console

---
+++ PartOne-TheBasics/Level08-Console2.0/TheDefenseOfConsolas/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Title = "Defense of Consolas";
+
+Console.WriteLine("Target row");
+var row = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Target Column: ");
+var column = Convert.ToInt32(Console.ReadLine());
+
+Console.ForegroundColor = ConsoleColor.Red;
+
+var firstPosition = $"({row}, {column-1})";
+var secondPosition = $"({row-1}, {column})";
+var thirdPosition = $"({row}, {column+1})";
+var fouthPosition = $"({row+1}, {column})";
+
+Console.WriteLine("Deploy to:");
+Console.WriteLine(firstPosition);
+Console.WriteLine(secondPosition);
+Console.WriteLine(thirdPosition);
+Console.WriteLine(fouthPosition);
+
+Console.Beep();
