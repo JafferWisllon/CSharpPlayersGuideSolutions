ca702a6bccc0592aeef7696e0a2a024fd45c70ac
Jaffer Wisllon <contatojaffer@gmail.com>
2026-08-24 17:56:11 -0300

    fix level name

---
+++ PartOne-TheBasics/Level06-TypeSystem/TheVariableShop/Program.cs
@@ -0,0 +1,55 @@
+﻿// byte
+byte age = 120;
+Console.WriteLine("Age: " + age);
+
+// sbyte
+sbyte amount = -10;
+Console.WriteLine("a negative small number: " + amount);
+
+// short
+short miles = -1500;
+Console.WriteLine("miles per hour: " + miles);
+
+// ushort
+ushort smallGamePoints = 24500; 
+Console.WriteLine("You point is : " + smallGamePoints);
+
+// uint
+uint likes = 312334;
+Console.WriteLine("Your photo has: " + likes + " likes");
+
+// int
+int intExample = -256877979;
+Console.WriteLine(intExample);
+
+// long
+long veryBigNegativeNumber = -15987546321;
+Console.WriteLine(veryBigNegativeNumber);
+
+// ulong
+long zeroLong = 0;
+Console.WriteLine(zeroLong);
+
+// char
+char emojii = '➕';
+Console.WriteLine(emojii);
+
+// string
+string greeting = "Hello";
+Console.WriteLine(greeting);
+
+// float
+float pi = 3.14159f;
+Console.WriteLine("Pi: " + pi);
+
+// double
+double longFloatingPoint = 12345.67890123456789;
+Console.WriteLine(longFloatingPoint);
+
+// decimal
+decimal cartAmount = 200.99m;
+Console.WriteLine("Cart amount: " + cartAmount);
+
+// bool
+bool isFalse = false;
+Console.WriteLine(isFalse);
---
+++ PartOne-TheBasics/Level06-TypeSystem/TheVariableShopReturns/Program.cs
@@ -0,0 +1,59 @@
+﻿// byte
+byte age = 120;
+// sbyte
+sbyte amount = -10;
+// short
+short miles = -1500;
+// ushort
+ushort smallGamePoints = 24500; 
+// uint
+uint likes = 312334;
+// int
+int intExample = -256877979;
+// long
+long veryBigNegativeNumber = -15987546321;
+// ulong
+long zeroLong = 0;
+// char
+char emojii = '➕';
+// string
+string greeting = "Hello";
+// float
+float pi = 3.14159f;
+// double
+double longFloatingPoint = 12345.67890123456789;
+// decimal
+decimal cartAmount = 200.99m;
+// bool
+bool isFalse = false;
+
+// update
+age = 80;
+amount = -11;
+miles = -100;
+smallGamePoints = 10000;
+likes = 9874;
+intExample = 787878;
+veryBigNegativeNumber = -888888888888;
+zeroLong = 0;
+emojii = '•';
+greeting = "Good morning";
+pi = 3.14159f;
+longFloatingPoint = 7878745.67890123456789;
+cartAmount = 129.99m;
+isFalse = true;
+
+Console.WriteLine("Age: " + age);
+Console.WriteLine("a negative small number: " + amount);
+Console.WriteLine("miles per hour: " + miles);
+Console.WriteLine("You point is : " + smallGamePoints);
+Console.WriteLine("Your photo has: " + likes + " likes");
+Console.WriteLine(intExample);
+Console.WriteLine(veryBigNegativeNumber);
+Console.WriteLine(zeroLong);
+Console.WriteLine(emojii);
+Console.WriteLine(greeting);
+Console.WriteLine("Pi: " + pi);
+Console.WriteLine(longFloatingPoint);
+Console.WriteLine("Cart amount: " + cartAmount);
+Console.WriteLine(isFalse);
