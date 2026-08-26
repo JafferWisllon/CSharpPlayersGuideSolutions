50acd0135aa64905c896cc0c9b3e1b474d98aa6e
Jaffer Wisllon <contatojaffer@gmail.com>
2026-08-26 19:19:48 -0300

    added solution level 9 decision making

---
+++ PartOne-TheBasics/Level09-DecisionMaking/RepairingTheClocktower/Program.cs
@@ -0,0 +1,7 @@
+﻿var number = Convert.ToInt32(Console.ReadLine());
+var isEven = (number % 2) == 0;
+
+if (isEven)
+    Console.WriteLine("Tick");
+else
+    Console.WriteLine("Tocks");
---
+++ PartOne-TheBasics/Level09-DecisionMaking/Watchtower/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("x: ");
+var x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("y: ");
+var y = Convert.ToInt32(Console.ReadLine());
+
+if (x < 0)
+{
+    if(y < 0)
+        Console.WriteLine("The enemy is to the NW");
+    else if(y == 0)
+        Console.WriteLine("The enemy is to the W");
+    else
+        Console.WriteLine("The enemy is to the SW");
+}
+else if (x == 0)
+{
+    if(y > 0)
+        Console.WriteLine("The enemy is to the N");
+    else if(y == 0)
+        Console.WriteLine("The enemy is here");
+    else
+        Console.WriteLine("The enemy is to the S");
+}
+else
+{
+    if(y > 0)
+        Console.WriteLine("The enemy is to the NE");
+    else if(y == 0)
+        Console.WriteLine("The enemy is to the E");
+    else
+        Console.WriteLine("The enemy is to the SE");
+}
