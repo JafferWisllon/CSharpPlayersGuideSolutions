e4133be592b0c608f6466f0ae08caf12972407ac
Jaffer Wisllon <contatojaffer@gmail.com>
2026-08-26 20:07:52 -0300

    added solution level 10 switch case

---
+++ PartOne-TheBasics/Level10-Switches/BuyingInventory/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("The following items are available:");
+Console.WriteLine("1 – Rope");
+Console.WriteLine("2 – Torches");
+Console.WriteLine("3 – Climbing Equipment");
+Console.WriteLine("4 – Clean Water");
+Console.WriteLine("5 – Machete");
+Console.WriteLine("6 – Canoe");
+Console.WriteLine("7 – Food Supplies");
+Console.Write("What number do you want to see the price of? ");
+
+var item = Convert.ToInt32(Console.ReadLine());
+
+string itemName = item switch
+{
+    1 => "Rope",
+    2 => "Torches",
+    3 => "Climbing Equipment",
+    4 => "Clean Water",
+    5 => "Machete",
+    6 => "Canoe",
+    7 => "Food Supplies"
+};
+
+int price = item switch
+{
+    1 => 10,
+    2 => 16,
+    3 => 24,
+    4 => 2,
+    5 => 20,
+    6 => 200,
+    7 => 2
+};
+
+Console.WriteLine($"{itemName} coast: {price} gold");
