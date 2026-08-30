f246cb8f667b3b66ba27ad2eaef6ae470c0f34ab
Jaffer Wisllon <contatojaffer@gmail.com>
2026-08-30 14:40:43 -0300

    added solution level 12 Arrays

---
+++ PartOne-TheBasics/Level12-Arrays/TheLawsOfFreach/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array =  new int[] {4, 51, -7, 13, -99, 15, -8, 45, 90};
+int smallest = int.MaxValue;
+
+foreach (var number in array)
+{
+    if (number < smallest)
+        smallest = number;
+}
+
+Console.WriteLine($"The smallest number: {smallest}");
+
+int sum = 0;
+foreach (var number in array)
+    sum += number;
+
+float average = (float)sum / array.Length;
+Console.WriteLine($"Average: {average}");
---
+++ PartOne-TheBasics/Level12-Arrays/TheReplicatorOfDTo/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] dtos = new int[5];
+
+for (int i = 0; i < dtos.Length; i++)
+{
+    Console.Write("Choose a number: ");
+    var number = Convert.ToInt32(Console.ReadLine());
+
+    dtos[i] = number;
+}
+
+int[] copy = new int[5];
+for(int i = 0; i < dtos.Length; i++)
+    copy[i] = dtos[i];
+
+for (int a = 0; a < copy.Length; a++)
+    Console.WriteLine($"Original: {dtos[a]} - Copy: {copy[a]}");
