8264d432e1944569b68059fa70c5eb3272e32c72
Jaffer Wisllon <contatojaffer@gmail.com>
2026-08-31 18:57:47 -0300

    added solution level 13 methods

---
+++ PartOne-TheBasics/Level13-Methods/Countdown/Program.cs
@@ -0,0 +1,8 @@
+﻿CountDown(10);
+
+void CountDown(int counter)
+{
+    if (counter <= 0) return;
+    Console.WriteLine(counter);
+    CountDown(counter - 1);
+}
---
+++ PartOne-TheBasics/Level13-Methods/TakingANumber/Program.cs
@@ -0,0 +1,21 @@
+﻿int num = AskForNunber("What is the airspeed velocity of an unladen swallow? ");
+Console.WriteLine(num);
+
+int numberInRange = AskForNumberInRange("What is the airspeed velocity of an unladen swallow? ", 10, 50);
+Console.WriteLine(numberInRange);
+int AskForNunber(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        var num = AskForNunber(text);
+        if (num <= min || num >= max)
+            continue;
+        return num;
+    }   
+}
