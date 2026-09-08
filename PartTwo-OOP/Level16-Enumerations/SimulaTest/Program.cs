3bfa442f6e680842cbae3b6bd16abfd7a0c99092
Jaffer Wisllon <contatojaffer@gmail.com>
2026-09-08 18:52:49 -0300

    added solution level 16 enumerations

---
+++ PartTwo-OOP/Level16-Enumerations/SimulaTest/Program.cs
@@ -0,0 +1,24 @@
+﻿ChestState currentState = ChestState.Locked;
+
+while (true)
+{
+    Console.Write($"The chest is {currentState}. What do you want to do? ");
+    var action = Console.ReadLine()!.Trim().ToLower();
+    
+    if (currentState == ChestState.Locked && action.Equals("unlock"))
+        currentState = ChestState.Closed;
+    else if (currentState == ChestState.Closed && action.Equals("open"))
+        currentState = ChestState.Open;
+    else if (currentState == ChestState.Open && action.Equals("close"))
+        currentState = ChestState.Closed;
+    else
+        break;
+}
+
+enum ChestState
+{
+    Locked,
+    Closed,
+    Open,
+}
+
