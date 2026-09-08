ad06a128995f0123a938d05cd28e3c0f7f132876
Jaffer Wisllon <contatojaffer@gmail.com>
2026-09-08 20:19:22 -0300

    added solution level17 Tuples

---
+++ PartTwo-OOP/Level17-Tuples/SimulaSoup/Program.cs
@@ -0,0 +1,56 @@
+﻿var type = AskType();
+var ingredient = AskMainIngredient();
+var seasoning = AskSeasoning();
+
+Console.WriteLine("Making soup...");
+DisplaySoup((type, ingredient, seasoning));
+
+SoupType AskType()
+{
+    Console.Write("Choose one soap type: [soup|stew|gumbo] ");
+    var choice = Console.ReadLine()!.Trim().ToLower();
+
+    return choice switch
+    {
+        "soup" => SoupType.Soup,
+        "stew" => SoupType.Stew,
+        "gumbo" => SoupType.Gumbo
+    };
+}
+
+MainIngredient AskMainIngredient()
+{
+    Console.Write("Choose main ingredient: [mushrooms|chicken|carrots|potatoes] ");
+    var choice = Console.ReadLine()!.Trim().ToLower();
+
+    return choice switch
+    {
+        "mushrooms" => MainIngredient.Mushrooms,
+        "chicken" => MainIngredient.Chicken,
+        "carrots" => MainIngredient.Carrots,
+        "potatoes" => MainIngredient.Potatoes
+    };
+}
+
+Seasoning AskSeasoning()
+{
+    Console.Write("Choose one soap type: [spicy|salty|sweet] ");
+    var choice = Console.ReadLine()!.Trim().ToLower();
+
+    return choice switch
+    {
+        "spicy" => Seasoning.Spicy,
+        "salty" => Seasoning.Salty,
+        "sweet" => Seasoning.Sweet
+    };
+}
+
+void DisplaySoup((SoupType type, MainIngredient ingredient, Seasoning seasoning) order) 
+    => Console.WriteLine($"{order.seasoning} {order.ingredient} {order.type}");
+
+enum SoupType { Soup, Stew, Gumbo }
+
+enum MainIngredient { Mushrooms, Chicken, Carrots, Potatoes }
+
+enum Seasoning { Spicy, Salty, Sweet }
+
